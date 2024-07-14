e3c29d8fbd0fa2750f387516cd9d4d40d932704a
kwhong95 <kwhong95@kakao.com>
2024-07-15 06:43:53 +0900

    [STORYBOOK]Initialize

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["selector"],
+  safelist: ["dark"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
